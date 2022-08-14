2781694c177be7eb626d5db27a613da3aa2acbee
GitHub Action <action@github.com>
2022-08-14 03:51:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/openexw/convto/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/openexw/convto/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.openexw.convto.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.openexw.convto.MyBundle
 
 class MyApplicationService {
 
